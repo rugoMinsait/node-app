d78c7e2fe687a00de4ee55093a56675c09b2e5d9
rugoMinsait <rgonzalezmar@minsait.com>
2024-05-17 01:52:19 +0200

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -7,5 +7,6 @@ const server = createServer((req, res) => {
 // starts a simple http server locally on port 3000
 server.listen(3010, '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3000');
+  console.log('App running...');
 });
 // run with `node server.mjs`
